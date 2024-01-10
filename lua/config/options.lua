0e59d156969da72c24c3821a52127ef3751f83d5
CodeSigils <web_tools88@yahoo.com>
2024-01-10 13:27:55 +0200

    disable netrw in options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+--- disable netrw - recommended for nvim-tree
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 -- This file is automatically loaded by plugins.core
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
