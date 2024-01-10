dce5de34292e2422491ac07a840373998167d2f9
CodeSigils <web_tools88@yahoo.com>
2024-01-10 13:28:42 +0200

    update conform plugin

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,9 +1,13 @@
 return {
   "stevearc/conform.nvim",
   -- :help conform-formatters
-  opts = {
-    format_on_save = {
-      lsp_fallback = true,
-    },
-  },
+  opts = function(_, opts)
+    opts.formatters_by_ft = {
+      lua = { "stylua" },
+      go = { "goimports" },
+      python = { "black" },
+      ---- Use a sub-list to run only the first available formatter
+      javascript = { { "biome", "prettierd", "prettier" } },
+    }
+  end,
 }
