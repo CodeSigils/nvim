c6babacb0299896894e02c2cdc405c5dcb2c5c00
CodeSigils <web_tools88@yahoo.com>
2024-01-10 11:52:01 +0200

    disable hardtime

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -1,12 +1,12 @@
 -- https://github.com/m4xshen/hardtime.nvim
 -- Disable with :Hardtime disable
 return {
-  "m4xshen/hardtime.nvim",
-  command = "Hardtime",
-  event = "BufEnter",
-  dependencies = {
-    "MunifTanjim/nui.nvim",
-    "nvim-lua/plenary.nvim",
-  },
-  opts = {},
+  -- "m4xshen/hardtime.nvim",
+  -- command = "Hardtime",
+  -- event = "BufEnter",
+  -- dependencies = {
+  --   "MunifTanjim/nui.nvim",
+  --   "nvim-lua/plenary.nvim",
+  -- },
+  -- opts = {},
 }
