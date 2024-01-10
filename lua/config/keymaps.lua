0732783555713eb516e7a7dfc2117dac52242187
CodeSigils <web_tools88@yahoo.com>
2024-01-10 13:27:35 +0200

    add some keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,19 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+
+-- Move to window using the <meta> hjkl keys
+map("n", "<m-h>", "<C-w>h", { desc = "Go to left window", remap = true })
+map("n", "<m-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
+map("n", "<m-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
+map("n", "<m-l>", "<C-w>l", { desc = "Go to right window", remap = true })
+
+-- windows
+map("n", "<leader>ww", "<C-W>p", { desc = "Other window", remap = true })
+map("n", "<leader>wd", "<C-W>c", { desc = "Delete window", remap = true })
+map("n", "<leader>wo", "<C-W>o", { desc = "Delete other windows", remap = true })
+map("n", "<leader>w-", "<C-W>s", { desc = "Split window below", remap = true })
+map("n", "<leader>w|", "<C-W>v", { desc = "Split window right", remap = true })
+map("n", "<leader>-", "<C-W>s", { desc = "Split window below", remap = true })
+map("n", "<leader>|", "<C-W>v", { desc = "Split window right", remap = true })
