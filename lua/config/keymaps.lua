33fcea0cbc96e8bd03cf2ee9d46d4d9f70cb3c67
CodeSigils <web_tools88@yahoo.com>
2024-01-11 17:27:12 +0200

    change window motion key from meta to ctrl

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,11 +3,18 @@
 -- Add any additional keymaps here
 local map = vim.keymap.set
 
--- Move to window using the <meta> hjkl keys
-map("n", "<m-h>", "<C-w>h", { desc = "Go to left window", remap = true })
-map("n", "<m-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
-map("n", "<m-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
-map("n", "<m-l>", "<C-w>l", { desc = "Go to right window", remap = true })
+-- Stay in indent mode
+map("v", "<", "<gv")
+map("v", ">", ">gv")
+
+-- last yank stays in the register
+map("x", "p", [["_dP]])
+
+-- Move to window using the <ctrl> hjkl keys
+map("n", "<C-h>", "<C-w>h", { desc = "Go to left window", remap = true })
+map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
+map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
+map("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
 
 -- windows
 map("n", "<leader>ww", "<C-W>p", { desc = "Other window", remap = true })
@@ -17,3 +24,7 @@ map("n", "<leader>w-", "<C-W>s", { desc = "Split window below", remap = true })
 map("n", "<leader>w|", "<C-W>v", { desc = "Split window right", remap = true })
 map("n", "<leader>-", "<C-W>s", { desc = "Split window below", remap = true })
 map("n", "<leader>|", "<C-W>v", { desc = "Split window right", remap = true })
+
+-- tailwind utils
+map({ "n", "x" }, "j", "gj")
+map({ "n", "x" }, "k", "gk")
