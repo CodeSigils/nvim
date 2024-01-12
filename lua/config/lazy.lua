8804d6290faf9bf773a799ffd0d4be98574eda52
CodeSigils <web_tools88@yahoo.com>
2024-01-12 10:25:17 +0200

    add aerial and markdown extras

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,6 +17,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.editor.aerial" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.vscode" },
     -- import/override with your plugins
     { import = "plugins" },
