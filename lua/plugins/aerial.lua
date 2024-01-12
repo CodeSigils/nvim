413dee34eab9ef6ac02b4fd0ff243990e57f475f
CodeSigils <web_tools88@yahoo.com>
2024-01-12 12:07:00 +0200

    change key to leader j for aerial

--- lua/plugins/aerial.lua
+++ lua/plugins/aerial.lua
@@ -5,4 +5,7 @@ return {
     backends = { "treesitter", "lsp", "markdown", "man", "lua" },
     default_direction = "prefer_right",
   },
+  keys = {
+    { "<leader>j", "<cmd>AerialToggle<cr>", desc = "Aerial (Symbols)" },
+  },
 }
