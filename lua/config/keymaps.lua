8a8d9d6d7d7f6a4fae0234105cac8c5919d6f5e5
CodeSigils <web_tools88@yahoo.com>
2024-01-31 13:26:41 +0200

    add MarkDownPreview and TZen keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -33,6 +33,13 @@ map({ "n", "x" }, "k", "gk")
 map({ "n", "x" }, "g.", "<cmd>Browse<cr>", { desc = "Browse URL", remap = true })
 
 -- Markdown preview
-map({ "n", "x" }, "mp", "<cmd>MarkdownPreview<cr>", { desc = " Markdown preview" })
-map({ "n", "x" }, "mp", "<cmd>MarkdownPreviewStop<cr>", { desc = " Markdown preview" })
-map({ "n", "x" }, "mt", "<cmd>MarkdownPreviewToggle<cr>", { desc = " Markdown preview" })
+map({ "n", "x" }, "mp", "<cmd>MarkdownPreview<cr>", { desc = "Markdown preview" })
+map({ "n", "x" }, "mp", "<cmd>MarkdownPreviewStop<cr>", { desc = "Markdown preview" })
+map({ "n", "x" }, "mt", "<cmd>MarkdownPreviewToggle<cr>", { desc = "Markdown preview" })
+
+-- zen mode: true-zen plugin
+-- map({ "n" }, "<leader>zn", "<cmd>TZNarrow<cr>", { desc = "Zen narrow" })
+-- map({ "v" }, "<leader>zn", "<cmd>'<,'>TZNarrow<cr>", { desc = "Zen narrow left-right" })
+-- map({ "n" }, "<leader>zf", "<cmd>TZFocus<cr>", { desc = "Zen focus" })
+-- map({ "n" }, "<leader>zm", "<cmd>TZMinimalist<cr>", { desc = "Zen minimalist" })
+-- map({ "n" }, "<leader>za", "<cmd>TZAtaraxis<cr>", { desc = "Zen ataraxis" })
