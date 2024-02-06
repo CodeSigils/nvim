20c889ab0bb6409a9f6e3314f2bdfe1dd1dad9fb
CodeSigils <web_tools88@yahoo.com>
2024-02-06 18:19:06 +0200

    rename plugins

---
+++ lua/plugins/format.lua
@@ -0,0 +1,44 @@
+local M = {
+  "lukas-reineke/lsp-format.nvim",
+}
+
+function M.config()
+  local prettier = {
+    tabWidth = function()
+      return vim.opt.shiftwidth:get()
+    end,
+    singleQuote = true,
+    trailingComma = "all",
+    configPrecedence = "prefer-file",
+    exclude = { "tsserver", "jsonls" },
+  }
+  require("lsp-format").setup({
+    typescript = prettier,
+    javascript = prettier,
+    typescriptreact = prettier,
+    javascriptreact = prettier,
+    json = prettier,
+    css = prettier,
+    scss = prettier,
+    html = prettier,
+    yaml = {
+      tabWidth = function()
+        return vim.opt.shiftwidth:get()
+      end,
+      singleQuote = true,
+      trailingComma = "all",
+      configPrecedence = "prefer-file",
+    },
+    python = {
+      lineLength = 120,
+    },
+    markdown = prettier,
+    sh = {
+      tabWidth = function()
+        return vim.opt.shiftwidth:get()
+      end,
+    },
+  })
+end
+
+return M
