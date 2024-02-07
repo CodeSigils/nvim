3ff5c20a4ad5e63f7c72434ead21e3734935eecf
CodeSigils <web_tools88@yahoo.com>
2024-02-07 10:32:08 +0200

    add servers to lsp

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -112,6 +112,11 @@ return {
       servers = {
         -- tsserver will be automatically installed with mason and loaded with lspconfig
         tsserver = {},
+        awkls = {},
+        astro = {},
+        bashls = {},
+        cssls = {},
+        intelephense = {},
       },
       -- you can do any additional lsp server setup here
       -- return true if you don't want this server to be setup with lspconfig
