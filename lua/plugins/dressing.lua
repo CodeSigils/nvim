3e5df3d99c89954a1b2fd9374415b26ef59848ae
CodeSigils <web_tools88@yahoo.com>
2024-02-22 18:50:37 +0200

    add dressing

--- lua/plugins/dressing.lua
+++ lua/plugins/dressing.lua
@@ -1,5 +1,42 @@
 -- hhtps://github.com/stevearc/dressing.nvim
 -- better vim.ui
+--
+local opts = {
+  input = {
+    trim_prompt = false,
+    -- Set to `false` to disable
+    mappings = {
+      n = {
+        ["<Esc>"] = "Close",
+        ["<CR>"] = "Confirm",
+      },
+      i = {
+        ["<C-c>"] = "Close",
+        ["<CR>"] = "Confirm",
+        ["<C-k>"] = "HistoryPrev",
+        ["<C-j>"] = "HistoryNext",
+      },
+    },
+    override = function(conf)
+      -- This is the config that will be passed to nvim_open_win.
+      -- Change values here to customize the layout
+      return conf
+    end,
+
+    -- see :help dressing_get_config
+    get_config = nil,
+  },
+  select = {
+    nui = {
+      position = "60%",
+      size = nil,
+    },
+  },
+}
+
 return {
   "stevearc/dressing.nvim",
+  config = function()
+    require("dressing").setup(opts)
+  end,
 }
