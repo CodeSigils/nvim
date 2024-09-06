7b706377969fb59c0ba0cb539cd83b75c4dfe084
CodeSigils <web_tools88@yahoo.com>
2024-09-06 11:41:52 +0300

    Update plugin
    Remove setup_ts_grammar

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,8 +1,37 @@
 return {
   "neovim/nvim-lspconfig",
+  ---@class PluginLspOpts
   opts = {
+    ---@type lspconfig.options
     servers = {
+      astro = {},
+      bashls = {},
+      cssls = {},
       marksman = {},
+      emmet_ls = {
+        filetypes = {
+          "html",
+          "typescriptreact",
+          "javascriptreact",
+          "css",
+          "sass",
+          "scss",
+          "svelte",
+          "less",
+          "javascript",
+          "typescript",
+          "markdown",
+          "ejs",
+        },
+      },
+      init_options = {
+        html = {
+          options = {
+            -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L26
+            ["bem.enabled"] = true,
+          },
+        },
+      },
     },
   },
 }
